1119b305dd50dc1d76e7aac05333517aa629e034
Sudipta Mitra <164529467+Sudipta-Mitra@users.noreply.github.com>
2025-08-07 12:38:02 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+
+# Load model
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/')
+def index():
+    return render_template('index.html')  # Renders templates/index.html
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    text = data['text']
+    prediction = model.predict([text])
+    return jsonify({'prediction': prediction[0]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
